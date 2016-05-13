631e09e2bc8f00df7084d6044de40da1d6e22c8a
JonathanEka <jonathan.eka.s@mail.ugm.ac.id>
2016-05-13 23:24:23 +0700

    Update tombol SIGN ME IN & +teks'doesn't have an account? signup' di SignIn side-menu

--- resources/views/layout/sidebar-login.blade.php
+++ resources/views/layout/sidebar-login.blade.php
@@ -4,7 +4,7 @@
       <a class="navbar-brand" href="#">Folksbeat Id</a>
     </div>
     <form role="form">
-      <div class="form-group">
+      <div class="form-group" align="center">
         <label><h3>Sign In</h3></label>
       </div>
       <div class="form-group">
@@ -16,7 +16,12 @@
       <div class="checkbox">
         <label><input type="checkbox"> Remember me</label>
       </div>
-      <button type="submit" class="btn btn-success btn-lg btn-block">Submit</button>
+      <button type="submit" class="btn btn-success btn-lg btn-block">SIGN ME IN</button>
     </form>
+    <div align="center">
+      <h6> Doesn't have an account?
+          <a href="http://www.google.com"><font color="orange">Sign Up</font></a>
+      </h6>
+    </div>
   </div>
 </div>
