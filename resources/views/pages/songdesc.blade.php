50f7bd473cf0a12079a427278794a518a4fc1af5
nourmareizky <nourmareizkyd@gmail.com>
2016-05-14 10:54:43 +0700

    add row

--- resources/views/pages/songdesc.blade.php
+++ resources/views/pages/songdesc.blade.php
@@ -3,11 +3,28 @@
 @section('content')
           <div class="container-fluid">
               <div class="row">
-                  <div class="col-lg-12">
-                      <h1>Content Song Desc Page</h1>
-                      <p>content</p>
-                  </div>
+                  <div class="col-md-12">
+                      <h1>Hymn For The Weekend</h1>
+                      <h2>Coldplay</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus est, si videtur,
+                       et recta quidem ad me. In his igitur partibus duabus nihil erat, quod Zeno commutare gestiret. 
+                       Duo Reges: constructio interrete. Optime, inquam. Iubet igitur nos Pythius Apollo noscere nosmet ipsos. 
+                       Hic ego: Pomponius quidem, inquam, noster iocari videtur,
+                       et fortasse suo iure. Immo vero, inquit, ad beatissime vivendum parum est, ad beate vero satis. Nam quid possumus facere melius? 
+                     </p>
+
               </div>
+              <div class="row">
+                  <div class="col-md-12">
+                      <h1>Content Song Desc Page</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus est, si videtur,
+                       et recta quidem ad me. In his igitur partibus duabus nihil erat, quod Zeno commutare gestiret. 
+                       Duo Reges: constructio interrete. Optime, inquam. Iubet igitur nos Pythius Apollo noscere nosmet ipsos. 
+                       Hic ego: Pomponius quidem, inquam, noster iocari videtur,
+                       et fortasse suo iure. Immo vero, inquit, ad beatissime vivendum parum est, ad beate vero satis. Nam quid possumus facere melius? 
+                     </p>
+                  </div>
+                </div>
           </div>
 
 @stop
