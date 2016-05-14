9f7847973bef8cc613bb26d7482eb95493b22574
JonathanEka <jonathan.eka.s@mail.ugm.ac.id>
2016-05-14 11:25:05 +0700

    tambah icon GooglePlay & AppStore di side-menu. Klik icon untuk Link ke playstore & appstore juga

--- resources/views/layout/sidebar-signup.blade.php
+++ resources/views/layout/sidebar-signup.blade.php
@@ -34,9 +34,14 @@
           <a href="http://www.google.com"><font color="orange">Sign In</font></a>
       </h6>
     </div>
-    <div>
-      <img src="../../image/logo_AppStore.png" alt="appstore" width="38" height="13">
+    <div align="center">
+      <a href="http://play.google.com">
+      <img src="https://raw.githubusercontent.com/shoddiqjati/folksbeat-dev/dev-frontend/resources/assets/image/logo_GooglePlayStore.png" alt="appstore" width="31%" height="9.1%" style="border:0;">
+      </a>
+      <span style="padding-right:15.74%;"> </span>
+      <a href="http://www.appstore.com">
+      <img src="https://raw.githubusercontent.com/shoddiqjati/folksbeat-dev/dev-frontend/resources/assets/image/logo_AppStore.png" alt="appstore" width="26.12%" height="9.1%" style="border:0;">
+      </a>
     </div>
-
   </div>
 </div>
