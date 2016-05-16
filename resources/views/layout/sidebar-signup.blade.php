f08e484e6d7f889faa7c924d26d16b723d5db6b6
JonathanEka <jonathan.eka.s@mail.ugm.ac.id>
2016-05-16 18:42:26 +0700

    revisi ukuran logo appstore+playstore

--- resources/views/layout/sidebar-signup.blade.php
+++ resources/views/layout/sidebar-signup.blade.php
@@ -36,11 +36,11 @@
     </div>
     <div align="center">
       <a href="http://play.google.com">
-      <img src="https://raw.githubusercontent.com/shoddiqjati/folksbeat-dev/dev-frontend/resources/assets/image/logo_GooglePlayStore.png" alt="appstore" width="31%" height="9.1%" style="border:0;">
+      <img src="https://raw.githubusercontent.com/shoddiqjati/folksbeat-dev/dev-frontend/resources/assets/image/logo_GooglePlayStore.png" alt="appstore" width="112.5" height="34.125" style="border:0;">
       </a>
       <span style="padding-right:15.74%;"> </span>
       <a href="http://www.appstore.com">
-      <img src="https://raw.githubusercontent.com/shoddiqjati/folksbeat-dev/dev-frontend/resources/assets/image/logo_AppStore.png" alt="appstore" width="26.12%" height="9.1%" style="border:0;">
+      <img src="https://raw.githubusercontent.com/shoddiqjati/folksbeat-dev/dev-frontend/resources/assets/image/logo_AppStore.png" alt="appstore" width="101.325" height="34.125" style="border:0;">
       </a>
     </div>
   </div>
